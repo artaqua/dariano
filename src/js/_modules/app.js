2bcfd072c21a5bb642d76e47670092777794cad6
artaqua <artaquan@gmail.com>
2020-02-08 12:22:16 +0200

    first commit

--- src/js/_modules/app.js
+++ src/js/_modules/app.js
@@ -88,110 +88,119 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  // // SlideToggle test
-  // document.querySelector('.testt').addEventListener('click', (event) => {
-  //   event.preventDefault();
-  //   slideToggle(document.querySelector(".testt"), 1000);
-  // }, false);
+  // SlideToggle category
+  const categoryHeadLink = document.querySelector('.category-head-link');
+  if (categoryHeadLink) {
+    categoryHeadLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      const category = document.querySelector('.category');
 
-  // Tabs
-  (function () {
-    const tabs = document.querySelectorAll('.tab');
-    if (tabs) {
-      tabs.forEach(tab => {
-        const dataTab = tab.querySelector('.tab__link_active').getAttribute('data-tab');
-        const contentItems = tab.querySelectorAll('.tab__content-item');
-        const tabLinks = tab.querySelectorAll('.tab__link');
-        const dataTitle = tab.querySelector('.tab__link_active').getAttribute('data-title');
+      if ( !category.classList.contains('category_active') ) {
+        category.classList.add('category_active');
+      } else {
+        category.classList.remove('category_active');
+      }
+    }, false);
+  }
 
-        // Init
-        contentItems.forEach(block => {
-          block.classList.remove('tab__content-item_active');
-        }, false);
-        tab.querySelector(dataTab).classList.add('tab__content-item_active');
+  // // Tabs
+  // (function () {
+  //   const tabs = document.querySelectorAll('.tab');
+  //   if (tabs) {
+  //     tabs.forEach(tab => {
+  //       const dataTab = tab.querySelector('.tab__link_active').getAttribute('data-tab');
+  //       const contentItems = tab.querySelectorAll('.tab__content-item');
+  //       const tabLinks = tab.querySelectorAll('.tab__link');
+  //       const dataTitle = tab.querySelector('.tab__link_active').getAttribute('data-title');
 
-        // Change tab
-        tabLinks.forEach(link => {
-          link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const clickedLink = event.currentTarget;
-            const clickedLinkData = clickedLink.getAttribute('data-tab');
-            const clickedLinkDataTitle = clickedLink.getAttribute('data-title');
+  //       // Init
+  //       contentItems.forEach(block => {
+  //         block.classList.remove('tab__content-item_active');
+  //       }, false);
+  //       tab.querySelector(dataTab).classList.add('tab__content-item_active');
 
-            tabLinks.forEach(link => {
-              link.classList.remove('tab__link_active');
-            }, false);
-            clickedLink.classList.add('tab__link_active');
+  //       // Change tab
+  //       tabLinks.forEach(link => {
+  //         link.addEventListener('click', (event) => {
+  //           event.preventDefault();
+  //           const clickedLink = event.currentTarget;
+  //           const clickedLinkData = clickedLink.getAttribute('data-tab');
+  //           const clickedLinkDataTitle = clickedLink.getAttribute('data-title');
 
-            contentItems.forEach(item => {
-              item.classList.remove('tab__content-item_active');
-            }, false);
-            tab.querySelector(clickedLinkData).classList.add('tab__content-item_active');
+  //           tabLinks.forEach(link => {
+  //             link.classList.remove('tab__link_active');
+  //           }, false);
+  //           clickedLink.classList.add('tab__link_active');
 
-          }, false);
-        }, false);
-      }, false);
-    }
-  })();
+  //           contentItems.forEach(item => {
+  //             item.classList.remove('tab__content-item_active');
+  //           }, false);
+  //           tab.querySelector(clickedLinkData).classList.add('tab__content-item_active');
 
-  // Animations
-  (function () {
-    const animServices = document.querySelector('.services__row');
+  //         }, false);
+  //       }, false);
+  //     }, false);
+  //   }
+  // })();
 
-    function doAnimate() {
-      anime({
-        targets: '.services__row, .services__col',
-        easing: 'easeInOutQuad',
-        opacity: [0,1],
-        rotateX: [-45,0],
-        translateY: [-20,0],
-        duration: 1000,
-        delay: anime.stagger(100, {start: 300})
-      });
-    }
+  // // Animations
+  // (function () {
+  //   const animServices = document.querySelector('.services__row');
 
-    // Когда доскролили до елемента начать анимацию
-    if ( animServices ) {
-      const waypoint = new Waypoint({
-        element: animServices,
-        handler: function(direction) {
-          if( direction === 'down' ) {
-            doAnimate();
-            // Animate once
-            this.destroy();
-          }
-        },
-        offset: '100%'
-      });
-    }
+  //   function doAnimate() {
+  //     anime({
+  //       targets: '.services__row, .services__col',
+  //       easing: 'easeInOutQuad',
+  //       opacity: [0,1],
+  //       rotateX: [-45,0],
+  //       translateY: [-20,0],
+  //       duration: 1000,
+  //       delay: anime.stagger(100, {start: 300})
+  //     });
+  //   }
 
-    const featuresSpecialCol = document.querySelector('.features-special');
+  //   // Когда доскролили до елемента начать анимацию
+  //   if ( animServices ) {
+  //     const waypoint = new Waypoint({
+  //       element: animServices,
+  //       handler: function(direction) {
+  //         if( direction === 'down' ) {
+  //           doAnimate();
+  //           // Animate once
+  //           this.destroy();
+  //         }
+  //       },
+  //       offset: '100%'
+  //     });
+  //   }
 
-    if (featuresSpecialCol) {
-      const waypoint = new Waypoint({
-        element: featuresSpecialCol,
-        handler: function(direction) {
-          if( direction === 'down' ) {
-            console.log('test');
-            anime({
-              targets: '.features-special__col',
-              easing: 'easeInOutSine',
-              scale: [0.8,1],
-              opacity: [0,1],
-              rotateX: [90,0],
-              translateY: ['-50%',0],
-              duration: 1200,
-              delay: anime.stagger(120, {start: 300})
-            });
+  //   const featuresSpecialCol = document.querySelector('.features-special');
+
+  //   if (featuresSpecialCol) {
+  //     const waypoint = new Waypoint({
+  //       element: featuresSpecialCol,
+  //       handler: function(direction) {
+  //         if( direction === 'down' ) {
+  //           console.log('test');
+  //           anime({
+  //             targets: '.features-special__col',
+  //             easing: 'easeInOutSine',
+  //             scale: [0.8,1],
+  //             opacity: [0,1],
+  //             rotateX: [90,0],
+  //             translateY: ['-50%',0],
+  //             duration: 1200,
+  //             delay: anime.stagger(120, {start: 300})
+  //           });
             
-            // Animate once
-            this.destroy();
-          }
-        },
-        offset: '100%'
-      });
-    }
-  })();
+  //           // Animate once
+  //           this.destroy();
+  //         }
+  //       },
+  //       offset: '100%'
+  //     });
+  //   }
+  // })();
   
 
 });
